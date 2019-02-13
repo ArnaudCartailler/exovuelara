bff1a80fe359d4b47a06e2ed1ec00a794821d2ba
arnaudcartailler <arnaud.cartailler@gmail.com>
2019-02-13 15:31:25 +0100

    calendar + nav + tree

---
+++ resources/js/components/AppHome.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <toolbar></toolbar>
+        <calendar></calendar>
+        <tree></tree>
+        <router-view></router-view>
+    </div>
+
+</template>
+
+<script>
+
+import toolbar from './Toolbar'
+import tree from './Tree'
+import calendar from './Calendar'
+
+export default {
+    components:{toolbar, tree, calendar}
+}
+
+</script>
+
+<style>
+
+</style>
